3224cbfc6af6e8f3eaea85d5f3020401833adf0e
Paul Magnus <pmagnusnh@gmail.com>
2016-12-08 21:16:55 +0100

    Added mat3 translation(dx, dy)

--- LinearAlgebra.h
+++ LinearAlgebra.h
@@ -9,7 +9,11 @@ typedef struct vec2 {
 
 	vec2(int x = 0, int y = 0) : x(x), y(y) {}
 
-	vec2 operator+(const vec2& v) {
+	bool operator==(const vec2& v) const {
+		return x == v.x && y == v.y;
+	}
+
+	vec2 operator+(const vec2& v) const {
 		return vec2(x + v.x, y + v.y);
 	}
 
@@ -19,7 +23,7 @@ typedef struct vec2 {
 		return *this;
 	}
 
-	vec2 operator-(const vec2& v) {
+	vec2 operator-(const vec2& v) const {
 		return vec2(x - v.x, y - v.y);
 	}
 
@@ -30,7 +34,7 @@ typedef struct vec2 {
 	}
 
 	/* Scalar multiplication of the vector */
-	vec2 operator*(const int s) {
+	vec2 operator*(const int s) const {
 		return vec2(x * s, y * s);
 	}
 
@@ -42,7 +46,7 @@ typedef struct vec2 {
 	}
 
 	/* Elementwise multiplication of the vector */
-	vec2 operator*(const vec2& v) {
+	vec2 operator*(const vec2& v) const {
 		return vec2(x * v.x, y * v.y);
 	}
 
@@ -60,12 +64,12 @@ typedef struct vec2 {
 	}
 	*/
 
-	double length() {
+	double length() const {
 		return sqrt(pow((float)x, 2) + pow((float)y, 2));
 	}
 
 	/* The dot product of two vectors*/
-	float dot(const vec2& v) {
+	float dot(const vec2& v) const {
 		return x * v.x + y * v.y;
 	}
 
@@ -80,7 +84,7 @@ typedef struct vec3 {
 	vec3(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
 
 	/* Addition of two matricies */
-	vec3 operator+(const vec3& v) {
+	vec3 operator+(const vec3& v) const {
 		return vec3(x + v.x, y + v.y, z + v.z);
 	}
 
@@ -93,7 +97,7 @@ typedef struct vec3 {
 	}
 
 	/* Scalar multiplication of the vector */
-	vec3 operator*(const double s) {
+	vec3 operator*(const double s) const {
 		return vec3(x * s, y * s, z * s);
 	}
 
@@ -106,7 +110,7 @@ typedef struct vec3 {
 	}
 
 	/* Elementwise multiplication of the vector */
-	vec3 operator*(const vec3& v) {
+	vec3 operator*(const vec3& v) const {
 		return vec3(x * v.x, y * v.y, z * v.z);
 	}
 
@@ -129,22 +133,29 @@ typedef struct vec3 {
 struct mat3 {
 	double m[3][3];
 
-	mat3(double m00, double m01, double m02,
-		double m10, double m11, double m12,
-		double m20, double m21, double m22) {
+	mat3(double m00 = 0.0, double m01 = 0.0, double m02 = 0.0,
+		double m10 = 0.0, double m11 = 0.0, double m12 = 0.0,
+		double m20 = 0.0, double m21 = 0.0, double m22 = 0.0) {
 		m[0][0] = m00; m[0][1] = m01; m[0][2] = m02;
 		m[1][0] = m10; m[1][1] = m11; m[1][2] = m12;
 		m[2][0] = m20; m[2][1] = m21; m[2][2] = m22;
 	}
 
-	vec3 operator*(const vec3& v) {
+	vec3 operator*(const vec3& v) const {
 		return vec3(m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z,
 			m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z,
 			m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z);
 	}
 
+	mat3 translation(double dx, double dy) {
+		return mat3(
+			1, 0, dx,
+			0, 1, dy,
+			0, 0, 1);
+	}
 
-	void print() {
+
+	void print() const {
 		printf("begin matrix\n");
 		printf("%f %f %f\n", m[0][0], m[0][1], m[0][2]);
 		printf("%f %f %f\n", m[1][0], m[1][1], m[1][2]);
